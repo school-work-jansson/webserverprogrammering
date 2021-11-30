c30242072129556b36e4d230a1d800bfadb4bd05
Linus Jansson <linus.jansson2000@gmail.com>
2021-11-30 13:58:12 +0100

    Ny puugift

---
+++ node_uppgifter/uppgift_file_system_two/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const colorData = require("./lib/farger.json")
+
+const file = process.cwd() + "/minaFarger.md";
+
+function checkForFile(p)
+{
+    if (fs.existsSync(p))
+        return true
+    else 
+        return fs.writeFileSync(p, '')
+}
+
+colorData.colorList.forEach(c => {
+    fileCheck = checkForFile(file)
+    
+    if (!fileCheck)
+    {
+        console.log("Something went wrong")
+        console.log(fileCheck)
+        process.exit();
+    }
+    
+    fs.appendFile(file, `${c.färg}: ${c.hex}\n`, err => {
+        if (err) throw err;
+    })
+
+})
