217126ca849a4103aa6b490fbcd353c7c8c57515
Linus Jansson <linus.jansson2000@gmail.com>
2022-01-13 10:34:55 +0100

    Lite flera routes

--- slutprojekt/src/server.ts
+++ slutprojekt/src/server.ts
@@ -12,8 +12,34 @@ server.get('/', (req, res) => {
     res.send(`<p>${info.desc}</p>`)
 })
 
+server.get('/about', (req, res) => { 
+  res.send(`<p>About</p>`)
+  
+})
+
+server.get('/contact', (req, res) => { 
+  res.send(`<p>contact</p>`)
+  
+})
+
+server.post('/send_message', (req, res) => { 
+  res.redirect('/contact')
+  console.log("User requested to send a mail")
+  
+})
+
+server.post('/login', (req, res) => { 
+  console.log("User requested login")
+  res.redirect('/')
+})
+
+server.post('/signout', (req, res) => { 
+  console.log("User requested Signout")
+  res.redirect('/')
+})
+
 
 const port = 3000
 server.listen(port, () => {
-  console.log(`MetaGram http://localhost:${port}`)
+  console.log(`Food is up and running http://localhost:${port}`)
 })
