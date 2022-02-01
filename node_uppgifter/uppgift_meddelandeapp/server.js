2ec02e3a59a18347e5cab9863adbc0e7c0b16c99
Linus Jansson <linus.jansson2000@gmail.com>
2022-02-01 13:05:07 +0100

    Connection med servern med socket

--- node_uppgifter/uppgift_meddelandeapp/server.js
+++ node_uppgifter/uppgift_meddelandeapp/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const app = express();
 
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 app.use(express.static('./static'))
 
@@ -11,18 +13,24 @@ app.use(bodyParser.json());
 
 let messsages = []
 
+io.on("connection", (socket) => {
+    console.log("Ny connection på socketen")
+})
+
 app.get("/meddelanden", (req, res) => {
     res.send(messsages)
 })
 
 app.post("/send_message", (req, res) => {
-    messsages.push({name: req.body.name, message: req.body.message})
+    let message = {name: req.body.name, message: req.body.message}
+    messsages.push(message)
     console.log(messsages)
     res.sendStatus(200)
+    io.emit("message", message)
 })
 
 const PORT = 8080
-app.listen(8080, err => {
+http.listen(PORT, err => {
     if (err) throw err
 
     console.log(`Servern är igång på: http://localhost:${PORT}`)
